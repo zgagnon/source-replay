3603d4e7aa0411772a67f97ecdc6ac5896a9cf5e
Zoe <none@none>
2013-06-09 15:45:32 -0600

    Add jquery insertion of the text.
    --HG--
    extra : rebase_source : 84e2530591f7357232ba899f58428a1273b40a9c

--- js/sr.js
+++ js/sr.js
@@ -4,4 +4,5 @@ $(window).load(function () {
     var source = $("#file-template").html();
     var template = Handlebars.compile(source);
     var html = template({file: "File.txt"});
+    $("#file").append(html);
 });
