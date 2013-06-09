32458beb8cdb532880e3858a959865d802e2b9fd
Zoe <none@none>
2013-06-09 15:41:20 -0600

    Fixing source line - had wrong template element.
    --HG--
    extra : rebase_source : 28bdef87bcbcf90a966d735e2b3cc5a772592a53

--- js/sr.js
+++ js/sr.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(window).load(function () {
-    var source = $("#entry-template").html();
+    var source = $("#file-template").html();
     var template = Handlebars.compile(source);
     var html = template({file: "File.txt"});
 });
