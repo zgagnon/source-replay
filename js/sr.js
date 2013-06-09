11f1d7259823939b48a82c7bafecef7acc03afa8
Zoe <none@none>
2013-06-09 16:07:20 -0600

    Adding click handler to star the replay.
    --HG--
    extra : rebase_source : 794f761da4c009dda69469f73bf7e6f7f6a38eb0

--- js/sr.js
+++ js/sr.js
@@ -5,8 +5,18 @@ var DEFAULT_END = "tip";
 
 
 $(window).load(function () {
+
     var source = $("#file-template").html();
     var template = Handlebars.compile(source);
-    var html = template({file: "File.txt"});
-    $("#file").append(html);
+
+    //Setting event handler for the start button
+    $("#start-replay").click(function (event)
+    {
+        var start = $("#from").val() || DEFAULT_START;
+        var end = $("#to").val() || DEFAULT_END;
+
+        var content = start + " - " + end;
+        var html = template({file: content});
+        $("#file").html(html);
+    });
 });
