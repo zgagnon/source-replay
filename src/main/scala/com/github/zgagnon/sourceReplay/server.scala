041a9b131c450981a8367d6c2975bfa65db1f674
Zoe <zoe@invisibledrygoods.com>
2013-06-22 08:27:27 -0600

    Adding basic webserver

---
+++ src/main/scala/com/github/zgagnon/sourceReplay/server.scala
@@ -0,0 +1,41 @@
+package com.github.zgagnon.sourceReplay
+
+import com.sun.jersey.spi.container.servlet.ServletContainer
+import javax.ws.rs.{Path, GET}
+import org.eclipse.jetty.server.{ServerConnector, Server}
+import org.eclipse.jetty.servlet.{ServletContextHandler, ServletHolder}
+
+
+/**
+ * @author Zoe Gagnon
+ * @version ${project.version}
+ * @since
+ */
+
+
+object WebRunner {
+  def main(args: Array[String]) {
+
+    val server = new Server(8080)
+    val connector = new ServerConnector(server)
+    server.addConnector(connector)
+
+    val holder:ServletHolder = new ServletHolder(classOf[ServletContainer])
+    holder.setInitParameter("com.sun.jersey.config.property.resourceConfigClass",
+                            "com.sun.jersey.api.core.PackagesResourceConfig")
+    holder.setInitParameter("com.sun.jersey.config.property.packages",
+                            "com.github.zgagnon.sourceReplay")
+    val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+    context.addServlet(holder, "/*")
+    server.start()
+    server.join()
+  }
+}
+
+@Path("/helloworld")
+class TestResource {
+  @GET
+  def hello() = {
+    "HELLO!!"
+  }
+}
